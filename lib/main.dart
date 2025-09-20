00385771ecf611f3d68255157ade6e5461b26805
EJ Rosialda <elijahejrosialda@gmail.com>
2025-09-20 17:46:02 +0800

    init comm

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:trial/practice_file.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Practice File',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: PracticeFile(),
+    );
+  }
+}
